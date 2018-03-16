ff5555340e764502216346e5977466dd3c0820c5
miiaevia <miiaevia@gmail.com>
2018-03-16 15:39:34 -0600

    removed getDealer method and test

--- src/com/skilldistillery/blackjack/BlackJackGame.java
+++ src/com/skilldistillery/blackjack/BlackJackGame.java
@@ -2,20 +2,34 @@ package com.skilldistillery.blackjack;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import com.skilldistillery.gameapp.common.Game;
 import com.skilldistillery.gameparticipants.common.Dealer;
 import com.skilldistillery.gameparticipants.common.Gambler;
 
 public class BlackJackGame extends Game {
+	static Scanner kb = new Scanner(System.in); 
 
 	public static void main(String[] args) {
-		getPlayers(); 
+		
+		BlackJackGame bjg = new BlackJackGame();
+		Dealer dealer = new Dealer();
+		List<Gambler> newPlayers = bjg.getGamblers(); 
+		bjg.startGame(dealer, newPlayers);
+	}
+
+	public List<Gambler> getGamblers() {
+		List<Gambler> gamblers = new ArrayList<>(); 
+//		System.out.print("How many players are there? ");
+//		int numPlayers = kb.nextInt(); 
+		Gambler Ron = new Gambler();
+		gamblers.add(Ron); 
+		return gamblers; 
+	}
+
+	public void startGame(Dealer dealer, List<Gambler> newPlayers) {
 		
 	}
 
-	public void getPlayers() {
-		List<Gambler> gamblers = new ArrayList<>(); 
-		Dealer dealer = new Dealer(); 
-	}
 }
--- tests/come/skilldistillery/blackjack/tests/BlackJackGameTests.java
+++ tests/come/skilldistillery/blackjack/tests/BlackJackGameTests.java
@@ -2,23 +2,34 @@ package come.skilldistillery.blackjack.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class BlackJackGameTests {
+import com.skilldistillery.blackjack.BlackJackGame;
+import com.skilldistillery.gameparticipants.common.Dealer;
+import com.skilldistillery.gameparticipants.common.Gambler;
 
+public class BlackJackGameTests {
+	
+	BlackJackGame bjg; 
 	@Before
 	public void setUp() throws Exception {
+		bjg = new BlackJackGame();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		bjg = null; 
 	}
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test 
+	public void test_bjg_List_Gamblers_not_null() {
+		List<Gambler> unexpected = null;
+		List<Gambler> actual = bjg.getGamblers(); 
+		assertNotSame(unexpected, actual);
 	}
-
+	
 }
